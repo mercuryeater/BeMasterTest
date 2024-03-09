aa3e07b9c1d84a2f2172643319788a45ada794a4
Federico <federicoc03@gmail.com>
2024-03-09 17:51:25 -0500

    feat: :sparkles: LoginDone

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,40 @@
-import "./Login.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import logo from "@assets/beMaster2.png";
+import db from "../../../db.json";
+import "./Login.scss";
+
 export default function Login() {
+  const navigate = useNavigate();
+  const validateEmail = (email) => {
+    const re = /^[a-zA-Z][\w-]*(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return re.test(email);
+  };
+  const [error, setError] = useState("");
+
   function userLogin(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const loginData = Object.fromEntries(formData);
+
+    const { email, password } = loginData;
+
+    if (!validateEmail(email)) {
+      setError("El email ingresado no es válido.");
+      return;
+    }
+
+    const user = db.users.find(
+      (user) => user.email === email && user.password === password
+    );
+
     console.log("Login attempt: " + JSON.stringify(loginData));
+    if (user) {
+      navigate("/home");
+    } else {
+      setError("Email o contraseña incorrectos.");
+    }
   }
 
   return (
@@ -14,12 +42,14 @@ export default function Login() {
       <img className="login__logo" src={logo} alt="logoBeMaster" />
       <div className="login__container">
         <form className="login__form" onSubmit={userLogin}>
+          {error && <p className="login__error">{error}</p>}
           <label htmlFor="email">Dirección de correo electrónico</label>
           <input
             type="email"
             placeholder="Ingrese su email"
             name="email"
             id="email"
+            required
           />
           <label htmlFor="password">Contraseña</label>
           <input
@@ -27,6 +57,7 @@ export default function Login() {
             placeholder="Ingrese su contraseña"
             name="password"
             id="password"
+            required
           />
           <button type="submit">INICIA SESIÓN</button>
         </form>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -13,12 +13,7 @@ const router = createBrowserRouter([
   {
     path: "login",
     element: <LoginPage />,
-    // loader: LoginLoader,
   },
-  //   {
-  //     path: "*",
-  //     element: <NotFound />,
-  //   },
 ]);
 
 export default router;
