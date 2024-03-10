d0898f3577e19f5b381e947e37ee8a63e8d1ee38
Federico <federicoc03@gmail.com>
2024-03-10 01:50:25 -0500

    feat: :sparkles: addHomeFixminorUI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,40 @@
+import db from "../../../db.json";
 import "./Home.scss";
 
 export default function Home() {
+  const testImg = db.categories.comedies[0].banner;
+  console.log(testImg);
+
   return (
-    <>
-      <h2>Categorias</h2>
-      <h1>Cinco cards de categorias</h1>
-    </>
+    <div className="home">
+      <div className="home__card">
+        <img src={testImg} alt="Card 1" />
+      </div>
+      <div className="home__card">
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5A9416D67DC9595496B2666087596EE64DE379272051BB854157C0D938BE2C26/scale?width=400&aspectRatio=1.78&format=png"
+          alt="Card 2"
+        />
+      </div>
+      <div className="home__card">
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5A9416D67DC9595496B2666087596EE64DE379272051BB854157C0D938BE2C26/scale?width=400&aspectRatio=1.78&format=png"
+          alt="Card 3"
+        />
+      </div>
+      <div className="home__card">
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5A9416D67DC9595496B2666087596EE64DE379272051BB854157C0D938BE2C26/scale?width=400&aspectRatio=1.78&format=png"
+          alt="Card 4"
+        />
+      </div>
+      <div className="home__card">
+        <img
+          src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5A9416D67DC9595496B2666087596EE64DE379272051BB854157C0D938BE2C26/scale?width=400&aspectRatio=1.78&format=png"
+          alt="Card 5"
+        />
+      </div>
+      <button onClick={() => console.log(testImg)}>asdasd</button>
+    </div>
   );
 }
